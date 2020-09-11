958c2ff3a8d5023d3a24f46d44a09a443da16097
eaglx <eaglx.programmer@gmail.com>
2020-09-11 21:00:00 +0200

    create start file

---
+++ VMPROTECT.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import Editor.main as editorModule
+
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+
+print("""\
+ _   ____  _______________ _____ _____ _____ _____ _____ 
+| | | |  \/  || ___ \ ___ \  _  |_   _|  ___/  __ \_   _|
+| | | | .  . || |_/ / |_/ / | | | | | | |__ | /  \/ | |  
+| | | | |\/| ||  __/|    /| | | | | | |  __|| |     | |  
+\ \_/ / |  | || |   | |\ \\ \_/ / | | | |___| \__/\ | |  
+ \___/\_|  |_/\_|   \_| \_|\___/  \_/ \____/ \____/ \_/  
+""")
+print("ver 0.1.200911.2011-dev")
+
+# Creating TK Container
+root = Tk()
+# Passing Root to Editor Class
+editorModule.Editor(root)
+# Root Window Looping
+root.mainloop()
