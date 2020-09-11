cd94559ace042308f785c83db18731c341b14d2a
eaglx <eaglx.programmer@gmail.com>
2020-09-11 20:59:15 +0200

    debugger init

---
+++ Debugger/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "../Machine/include/opcodes.hpp"
+
+int main()
+{
+    std::cout << "VMPROTECT-DEBUGGER" << std::endl;
+    std::cout << "ver 0.1.200911.2056" << std::endl;
+
+
+    std::cout << "!!!!! NOT READY !!!!!" << std::endl;
+
+    return 0;
+}
