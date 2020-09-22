a5acb4c4c5a3db5c205698920cb250df95f034c9
eaglx <eaglx.programmer@gmail.com>
2020-09-22 21:36:16 +0200

    update install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -14,7 +14,45 @@ cat << "EOF"
  /__/\___|\__|\_,_| .__/ |_|_||_/__/\__\__,_|_|_\___|_|                                         
                   |_|                                                                           
 EOF
-echo "version x.x"
+echo "version 0.2.200921.2026"
+echo "#####################################"
+
+echo ""
+echo "Write path to deploy"
+read pathToInstall
+
+# #####################################
+# ############## STAGE 1 ##############
+echo "STAGE 1 - check if require programs exist"
+if ! [ -x "$(command -v python3)" ];
+then
+        echo "  WARNING: python3 could not be found"
+else
+        echo "  python3 installed - check"
+fi
+
+if ! [ -x "$(command -v make)" ];
+then
+        echo "  WARNING: make could not be found"
+else
+        echo "  make installed - check"
+fi
+
+if ! [ -x "$(command -v g++)" ];
+then
+        echo "  WARNING: g++ could not be found"
+else
+        echo "  g++ installed - check"
+fi
+
+if ! [ -x "$(command -v nasm)" ];
+then
+        echo "  WARNING: nasm could not be found"
+else
+        echo "  nasm installed - check"
+fi
+# #####################################
+
 
 echo "\e[1;31m!!!! NOT READY !!!!\e[0m"
 
